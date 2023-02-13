73be23a48af0b49a54de5f472bd6f018a1913198
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2023-02-14 02:20:27 +0530

    App working properly

---
+++ app/src/main/java/com/qwk/chandrsekhar/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,20 @@
+package com.qwk.chandrsekhar.ui.favorite
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.qwk.chandrsekhar.R
+
+class FavoriteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_saved, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/qwk/chandrsekhar/ui/favorite/FavoriteViewModel.kt
@@ -0,0 +1,7 @@
+package com.qwk.chandrsekhar.ui.favorite
+
+import androidx.lifecycle.ViewModel
+
+class SavedViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
