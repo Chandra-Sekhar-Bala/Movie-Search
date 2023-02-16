53ba1e8a857ef8cdb23021b4ee796736edf2fd23
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2023-02-17 00:52:37 +0530

    Made layout colors compatible for night mode.

--- app/src/main/java/com/qwk/chandrsekhar/MainActivity.kt
+++ app/src/main/java/com/qwk/chandrsekhar/MainActivity.kt
@@ -2,10 +2,20 @@ package com.qwk.chandrsekhar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.qwk.chandrsekhar.R
+import com.qwk.chandrsekhar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       val  binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // setup navigation
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavigation.setupWithNavController(navController)
+
     }
 }
